9e029f944d3380200f7bb5c596d99c5361db8785
AcruxTech <acrux_tech@mail.ru>
2022-10-26 00:17:29 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,59 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
 const config = {
+    mode: 'production',
+    target: 'web',
+    performance: {
+        hints: false,
+    },
+    optimization: {
+        splitChunks: {
+            minSize: 10000,
+            maxSize: 250000,
+        },
+    },
     entry: {
+        main: './src/index.js',
         about: './src/pages/About.js',
         home: './src/pages/Home.js',
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
         new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
+            saveReportTo: './report-[name]-[hash].html',
+            saveStatsTo: './stats-[name]-[hash].json',
             saveOnlyStats: false,
             open: false,
+            name: "shri_simple_template"
         }),
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
+        clean: true,
     },
     module: {
-        rules: [
-            // @TODO js rule
-            // @TODO css rule
+        rules: [                                       
+            {
+                test: /\.js?$/,                       
+                exclude: /(node_modules)/,             
+                loader: 'babel-loader',                
+                options: {
+                    presets: [
+                       ['@babel/preset-react', {"runtime": "automatic"}]
+                    ], 
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { 
+                test: /\.(html)$/, 
+                use: ['html-loader'] 
+            },
         ],
     },
     // @TODO optimizations
