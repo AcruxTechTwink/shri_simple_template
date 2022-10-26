eb0bc99a6c8daceddc8e00d8515aed22d86f4761
AcruxTech <78689853+AcruxTech@users.noreply.github.com>
2022-10-26 20:08:20 +0000

    Deploying to pages from @ AcruxTech/shri_simple_template@17a5706ad008186abfb541e5048bb70d0b066324 🚀

---
+++ src/utils/uuid.js
@@ -0,0 +1,8 @@
+// import crypto from 'crypto-browserify';
+
+// // генерируем какой-то идентификатор для запуска
+// export const UUID = crypto.randomUUID();
+
+import { v4 as uuidv4 } from 'uuid';
+
+export const UUID = uuidv4();
