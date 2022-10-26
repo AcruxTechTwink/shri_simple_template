97c1c93177181059519d975f7ce0433f0996a95e
AcruxTech <acrux_tech@mail.ru>
2022-10-26 15:49:21 +0300

    Updates

--- src/utils/uuid.js
+++
@@ -1,8 +0,0 @@
-// import crypto from 'crypto-browserify';
-
-// // генерируем какой-то идентификатор для запуска
-// export const UUID = crypto.randomUUID();
-
-import { v4 as uuidv4 } from 'uuid';
-
-export const UUID = uuidv4();
